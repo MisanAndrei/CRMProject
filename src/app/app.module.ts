a50e6f0f14f2cf5f7acd2cc5840be916b9f9b70c
Misan Andrei <misan.andrei@yahoo.com>
2024-05-29 19:56:49 +0300

    removed keycloak from master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-//import { initializeKeycloak } from './app-init';
-
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
@@ -45,8 +42,6 @@ import { TransfersComponent } from './Components/transfers/transfers-component/t
 import { AccountUpsertComponent } from './Components/accounts/account-upsert-component/account-upsert.component';
 import { PartnerUpsertComponent } from './Components/partners/partner-upsert-component/partner-upsert.component';
 import { TransactionUpsertComponent } from './Components/transactions/transaction-upsert-component/transaction-upsert.component';
-//import { TokenInterceptor } from './interceptors/token.interceptor';
-//import { AuthService } from './Services/auth.service';
 import { ApiService } from './Services/ApiService';
 import { TaxesUpsertComponent } from './Components/taxes/taxes-upsert-component/taxes-upsert/taxes-upsert.component';
 import { CategoriesUpsertComponent } from './Components/categories/categories-upsert-component/categories-upsert/categories-upsert.component';
