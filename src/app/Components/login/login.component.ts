259f60de7fd2315ce0772db95cc85a45f4d2dc3f
Misan Andrei <misan.andrei@yahoo.com>
2024-05-30 15:41:46 +0300

    change login component

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../../Services/auth.service';
 import { ApiService } from '../../Services/ApiService';
 import { Organization } from '../../Utilities/Models';
@@ -9,13 +11,19 @@ import { Organization } from '../../Utilities/Models';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
+  loginForm: FormGroup;
 
-  constructor(private authService: AuthService, private apiService: ApiService) {}
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService, private apiService: ApiService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      license: ['', [Validators.required]],
+      rememberMe: [false]
+    });
+  }
 
-  login(): void {
-    this.authService.customLogin(this.username, this.password).then(() => {
+  onSubmit(): void {
+    this.authService.customLogin(this.loginForm.get('email')?.value, this.loginForm.get('password')?.value).then(() => {
       console.log('Logged in');
       this.apiService.getOrganization().subscribe({
         next: (data: Organization) => {
@@ -32,9 +40,6 @@ export class LoginComponent {
     }).catch((error) => {
       console.error('Login failed', error);
     });
-  }
 
-  logOut(){
-    this.authService.logout();
-  }
+}
 }
