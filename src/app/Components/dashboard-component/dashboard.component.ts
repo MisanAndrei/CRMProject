84cf8f5f7c5b1865c734618d949b262b444656e9
Pacurar Cristina <dancristina95@yahoo.com>
2024-07-07 23:04:48 +0300

    ui-upades

--- src/app/Components/dashboard-component/dashboard.component.ts
+++ src/app/Components/dashboard-component/dashboard.component.ts
@@ -1,8 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartData, ChartOptions } from 'chart.js';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core'
+import { ChartData, ChartOptions } from 'chart.js'
+import { FormBuilder, FormGroup } from '@angular/forms'
 
-import { AggregatedStatistics, CashFlowItem, IncomeByCategoryItem, ExpensesByCategoryItem, Receivables, Payables, AccountSoldItem } from '../../Utilities/Models';
+import {
+  AggregatedStatistics,
+  CashFlowItem,
+  IncomeByCategoryItem,
+  ExpensesByCategoryItem,
+  Receivables,
+  Payables,
+  AccountSoldItem,
+} from '../../Utilities/Models'
 
 @Component({
   selector: 'app-dashboard',
@@ -12,9 +20,9 @@ import { AggregatedStatistics, CashFlowItem, IncomeByCategoryItem, ExpensesByCat
 export class DashboardComponent implements OnInit {
   aggregatedStatistics: AggregatedStatistics = {
     accountsSold: [
-      { accountName: 'Account A', totalSold: 1000 },
-      { accountName: 'Account B', totalSold: 1500 },
-      { accountName: 'Account C', totalSold: 2000 },
+      { accountName: 'Cont A', totalSold: 1000 },
+      { accountName: 'Cont B', totalSold: 1500 },
+      { accountName: 'Cont C', totalSold: 2000 },
     ],
     receivables: {
       totalToReceive: 5000,
@@ -27,82 +35,157 @@ export class DashboardComponent implements OnInit {
       totalOverdue: 1500,
     },
     cashFlow: [
-      { month: 'December', year: 2023, income: 11000, expenses: 7000 },
-      { month: 'January', year: 2024, income: 12000, expenses: 8000 },
-      { month: 'February', year: 2024, income: 13000, expenses: 9000 },
-      { month: 'March', year: 2024, income: 14000, expenses: 10000 },
-      { month: 'April', year: 2024, income: 15000, expenses: 11000 },
-      { month: 'May', year: 2024, income: 16000, expenses: 12000 },
-      { month: 'June', year: 2024, income: 17000, expenses: 13000 },
-      { month: 'July', year: 2024, income: 18000, expenses: 14000 },
-      { month: 'August', year: 2024, income: 19000, expenses: 15000 },
-      { month: 'September', year: 2024, income: 20000, expenses: 16000 },
-      { month: 'October', year: 2024, income: 21000, expenses: 17000 },
-      { month: 'November', year: 2024, income: 22000, expenses: 18000 },
-      { month: 'December', year: 2024, income: 23000, expenses: 19000 },
+      {
+        month: 'Decembrie',
+        year: 2023,
+        income: 11000,
+        expenses: 7000,
+      },
+      {
+        month: 'Ianuarie',
+        year: 2024,
+        income: 12000,
+        expenses: 8000,
+      },
+      {
+        month: 'Februarie',
+        year: 2024,
+        income: 13000,
+        expenses: 9000,
+      },
+      {
+        month: 'Martie',
+        year: 2024,
+        income: 13000,
+        expenses: 9000,
+      },
+      {
+        month: 'Aprilie',
+        year: 2024,
+        income: 13000,
+        expenses: 9000,
+      },
+      {
+        month: 'Mai',
+        year: 2024,
+        income: 13000,
+        expenses: 9000,
+      },
+      {
+        month: 'Iunie',
+        year: 2024,
+        income: 13000,
+        expenses: 9000,
+      },
+      {
+        month: 'Iulie',
+        year: 2024,
+        income: 13000,
+        expenses: 9000,
+      },
+      {
+        month: 'August',
+        year: 2024,
+        income: 13000,
+        expenses: 9000,
+      },
+      {
+        month: 'Septembrie',
+        year: 2024,
+        income: 13000,
+        expenses: 9000,
+      },
+      {
+        month: 'Octombrie',
+        year: 2024,
+        income: 13000,
+        expenses: 9000,
+      },
+      {
+        month: 'Noiembrie',
+        year: 2024,
+        income: 13000,
+        expenses: 9000,
+      },
     ],
     incomeByCategory: [
-      { categoryName: 'Category 1', colorCode: '#FF6384', income: 5000 },
-      { categoryName: 'Category 2', colorCode: '#36A2EB', income: 7000 },
-      { categoryName: 'Category 3', colorCode: '#FFCE56', income: 9000 },
+      { categoryName: 'Categoria 1', colorCode: '#E5E0FF', income: 5000 },
+      { categoryName: 'Categoria 2', colorCode: '#B6BBC4', income: 7000 },
+      { categoryName: 'Categoria 3', colorCode: '#CDE8E5', income: 9000 },
     ],
     expensesByCategory: [
-      { categoryName: 'Category 1', colorCode: '#FF6384', expenses: 4000 },
-      { categoryName: 'Category 2', colorCode: '#36A2EB', expenses: 6000 },
-      { categoryName: 'Category 3', colorCode: '#FFCE56', expenses: 8000 },
+      { categoryName: 'Categoria 1', colorCode: '#B9B4C7', expenses: 5000 },
+      { categoryName: 'Categoria 2', colorCode: '#BBE9FF', expenses: 7000 },
+      { categoryName: 'Categoria 3', colorCode: '#B1AFFF', expenses: 5000 },
     ],
-  };
+  }
 
-  // Bar chart options and data for CashFlow
   public barChartOptions: ChartOptions<'bar'> = {
     responsive: true,
-  };
+  }
+
   public barChartData: ChartData<'bar'> = {
-    labels: this.aggregatedStatistics.cashFlow.map(cf => cf.month),
+    labels: this.aggregatedStatistics.cashFlow.map((cf) => cf.month),
     datasets: [
-      { data: this.aggregatedStatistics.cashFlow.map(cf => cf.income), label: 'Income' },
-      { data: this.aggregatedStatistics.cashFlow.map(cf => cf.expenses), label: 'Expenses' }
+      {
+        data: this.aggregatedStatistics.cashFlow.map((cf) => cf.income),
+        label: 'Venituri',
+        backgroundColor: '#8B93FF',
+      },
+      {
+        data: this.aggregatedStatistics.cashFlow.map((cf) => cf.expenses),
+        label: 'Cheltuieli',
+        backgroundColor: '#FB7185',
+      },
     ],
-  };
+  }
 
   // Pie chart options and data for Income by Category
   public pieChartIncomeOptions: ChartOptions<'pie'> = {
     responsive: true,
-  };
+  }
   public pieChartIncomeData: ChartData<'pie'> = {
-    labels: this.aggregatedStatistics.incomeByCategory.map(ic => ic.categoryName),
+    labels: this.aggregatedStatistics.incomeByCategory.map(
+      (ic) => ic.categoryName,
+    ),
     datasets: [
       {
-        data: this.aggregatedStatistics.incomeByCategory.map(ic => ic.income),
-        backgroundColor: this.aggregatedStatistics.incomeByCategory.map(ic => ic.colorCode),
-      }
+        data: this.aggregatedStatistics.incomeByCategory.map((ic) => ic.income),
+        backgroundColor: this.aggregatedStatistics.incomeByCategory.map(
+          (ic) => ic.colorCode,
+        ),
+      },
     ],
-  };
+  }
 
   // Pie chart options and data for Expenses by Category
   public pieChartExpensesOptions: ChartOptions<'pie'> = {
     responsive: true,
-  };
+  }
   public pieChartExpensesData: ChartData<'pie'> = {
-    labels: this.aggregatedStatistics.expensesByCategory.map(ec => ec.categoryName),
+    labels: this.aggregatedStatistics.expensesByCategory.map(
+      (ec) => ec.categoryName,
+    ),
     datasets: [
       {
-        data: this.aggregatedStatistics.expensesByCategory.map(ec => ec.expenses),
-        backgroundColor: this.aggregatedStatistics.expensesByCategory.map(ec => ec.colorCode),
-      }
+        data: this.aggregatedStatistics.expensesByCategory.map(
+          (ec) => ec.expenses,
+        ),
+        backgroundColor: this.aggregatedStatistics.expensesByCategory.map(
+          (ec) => ec.colorCode,
+        ),
+      },
     ],
-  };
+  }
 
   dateRangeForm: FormGroup = this.fb.group({
     startDate: [''],
-    endDate: ['']
-  });
+    endDate: [''],
+  })
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
 
-  onSubmit(){
-
-  }
+  onSubmit() {}
 }
