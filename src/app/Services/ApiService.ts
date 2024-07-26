658841de4e55f445ce86d217dcd5f67b27e29215
Misan Andrei <misan.andrei@yahoo.com>
2024-07-26 12:38:03 +0300

    change in api service

--- src/app/Services/ApiService.ts
+++ src/app/Services/ApiService.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,27 +10,44 @@ import { Observable } from 'rxjs';
 export class ApiService {
   private apiUrl = 'https://backend-crm.efcon.ro/crm/api/v1';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   private getHeaders(): HttpHeaders {
     const token = this.getToken();
     return new HttpHeaders().set('Authorization', `Bearer ${token}`);
   }
 
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.status === 401) {
+      // Handle 401 Unauthorized response
+      localStorage.clear();
+      this.router.navigate(['/login']);
+    }
+    return throwError(error);
+  }
+
   get<T>(endpoint: string, params?: HttpParams): Observable<T> {
-    return this.http.get<T>(`${this.apiUrl}/${endpoint}`, { params, headers: this.getHeaders() });
+    return this.http.get<T>(`${this.apiUrl}/${endpoint}`, { params, headers: this.getHeaders() }).pipe(
+      catchError(this.handleError.bind(this))
+    );
   }
 
   post<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.post<T>(`${this.apiUrl}/${endpoint}`, body, { headers: this.getHeaders() });
+    return this.http.post<T>(`${this.apiUrl}/${endpoint}`, body, { headers: this.getHeaders() }).pipe(
+      catchError(this.handleError.bind(this))
+    );
   }
 
   put<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.put<T>(`${this.apiUrl}/${endpoint}`, body, { headers: this.getHeaders() });
+    return this.http.put<T>(`${this.apiUrl}/${endpoint}`, body, { headers: this.getHeaders() }).pipe(
+      catchError(this.handleError.bind(this))
+    );
   }
 
   delete<T>(endpoint: string): Observable<T> {
-    return this.http.delete<T>(`${this.apiUrl}/${endpoint}`, { headers: this.getHeaders() });
+    return this.http.delete<T>(`${this.apiUrl}/${endpoint}`, { headers: this.getHeaders() }).pipe(
+      catchError(this.handleError.bind(this))
+    );
   }
 
   getToken() {
