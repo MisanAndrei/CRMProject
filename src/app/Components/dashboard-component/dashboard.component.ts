bab8858689ffaf8179ca1005649c6f2925344ddf
Vlad <mv.muresanvlad@gmail.com>
2024-07-26 14:32:40 +0300

    dashboard integration

--- src/app/Components/dashboard-component/dashboard.component.ts
+++ src/app/Components/dashboard-component/dashboard.component.ts
@@ -11,8 +11,11 @@ import {
   Payables,
   AccountSoldItem,
   Partner,
+  CashFlowByPartner,
 } from '../../Utilities/Models'
 import { ApiService } from '../../Services/ApiService'
+import { firstValueFrom } from 'rxjs/internal/firstValueFrom'
+import { HttpParams } from '@angular/common/http'
 
 @Component({
   selector: 'app-dashboard',
@@ -21,216 +24,26 @@ import { ApiService } from '../../Services/ApiService'
 })
 export class DashboardComponent implements OnInit {
   selectedPartnerId!: number;
-  partners: Partner[] = [
-    {
-      id: 1,
-      name: 'Partner A',
-      cui: 'CUI12345',
-      regCom: 'RegCom123',
-      email: 'partnerA@example.com',
-      phoneNumber: '123456789',
-      country: 'Country A',
-      website: 'www.partnerA.com',
-      reference: 'RefA',
-      address: 'Address A',
-      city: 'City A',
-      county: 'County A',
-      postalCode: '12345',
-      image: 'path/to/imageA.png',
-    },
-    {
-      id: 2,
-      name: 'Partner B',
-      cui: 'CUI67890',
-      regCom: 'RegCom678',
-      email: 'partnerB@example.com',
-      phoneNumber: '987654321',
-      country: 'Country B',
-      website: 'www.partnerB.com',
-      reference: 'RefB',
-      address: 'Address B',
-      city: 'City B',
-      county: 'County B',
-      postalCode: '67890',
-      image: 'path/to/imageB.png',
-    },
-    {
-      id: 3,
-      name: 'Partner C',
-      cui: 'CUI54321',
-      regCom: 'RegCom543',
-      email: 'partnerC@example.com',
-      phoneNumber: '456789123',
-      country: 'Country C',
-      website: 'www.partnerC.com',
-      reference: 'RefC',
-      address: 'Address C',
-      city: 'City C',
-      county: 'County C',
-      postalCode: '54321',
-      image: 'path/to/imageC.png',
-    },
-  ];
-  aggregatedStatistics: AggregatedStatistics = {
-    accountsSold: [
-      { accountName: 'Cont A', totalSold: 1000 },
-      { accountName: 'Cont B', totalSold: 1500 },
-      { accountName: 'Cont C', totalSold: 2000 },
-    ],
-    receivables: {
-      totalToReceive: 5000,
-      totalOpen: 3000,
-      totalOverdue: 2000,
-    },
-    payables: {
-      totalToPay: 4000,
-      totalOpen: 2500,
-      totalOverdue: 1500,
-    },
-    cashFlow: [
-      {
-        month: 'Decembrie',
-        year: 2023,
-        income: 11000,
-        expenses: 7000,
-      },
-      {
-        month: 'Ianuarie',
-        year: 2024,
-        income: 12000,
-        expenses: 8000,
-      },
-      {
-        month: 'Februarie',
-        year: 2024,
-        income: 13000,
-        expenses: 9000,
-      },
-      {
-        month: 'Martie',
-        year: 2024,
-        income: 13000,
-        expenses: 9000,
-      },
-      {
-        month: 'Aprilie',
-        year: 2024,
-        income: 13000,
-        expenses: 9000,
-      },
-      {
-        month: 'Mai',
-        year: 2024,
-        income: 13000,
-        expenses: 9000,
-      },
-      {
-        month: 'Iunie',
-        year: 2024,
-        income: 13000,
-        expenses: 9000,
-      },
-      {
-        month: 'Iulie',
-        year: 2024,
-        income: 13000,
-        expenses: 9000,
-      },
-      {
-        month: 'August',
-        year: 2024,
-        income: 13000,
-        expenses: 9000,
-      },
-      {
-        month: 'Septembrie',
-        year: 2024,
-        income: 13000,
-        expenses: 9000,
-      },
-      {
-        month: 'Octombrie',
-        year: 2024,
-        income: 13000,
-        expenses: 9000,
-      },
-      {
-        month: 'Noiembrie',
-        year: 2024,
-        income: 13000,
-        expenses: 9000,
-      },
-    ],
-    incomeByCategory: [
-      { categoryName: 'Categoria 1', colorCode: '#E5E0FF', income: 5000 },
-      { categoryName: 'Categoria 2', colorCode: '#B6BBC4', income: 7000 },
-      { categoryName: 'Categoria 3', colorCode: '#CDE8E5', income: 9000 },
-    ],
-    expensesByCategory: [
-      { categoryName: 'Categoria 1', colorCode: '#B9B4C7', expenses: 5000 },
-      { categoryName: 'Categoria 2', colorCode: '#BBE9FF', expenses: 7000 },
-      { categoryName: 'Categoria 3', colorCode: '#B1AFFF', expenses: 5000 },
-    ],
-    cashFlowByPartner: {partnerName: 'partener', totalPaid: 1234, totalReceived: 1234}
-  }
+  partners: Partner[] = [];
+  aggregatedStatistics!: AggregatedStatistics;
 
   public barChartOptions: ChartOptions<'bar'> = {
     responsive: true,
   }
 
-  public barChartData: ChartData<'bar'> = {
-    labels: this.aggregatedStatistics.cashFlow.map((cf) => cf.month),
-    datasets: [
-      {
-        data: this.aggregatedStatistics.cashFlow.map((cf) => cf.income),
-        label: 'Venituri',
-        backgroundColor: '#8B93FF',
-      },
-      {
-        data: this.aggregatedStatistics.cashFlow.map((cf) => cf.expenses),
-        label: 'Cheltuieli',
-        backgroundColor: '#FB7185',
-      },
-    ],
-  }
+  public barChartData!: ChartData<'bar'>;
 
   // Pie chart options and data for Income by Category
   public pieChartIncomeOptions: ChartOptions<'pie'> = {
     responsive: true,
   }
-  public pieChartIncomeData: ChartData<'pie'> = {
-    labels: this.aggregatedStatistics.incomeByCategory.map(
-      (ic) => ic.categoryName,
-    ),
-    datasets: [
-      {
-        data: this.aggregatedStatistics.incomeByCategory.map((ic) => ic.income),
-        backgroundColor: this.aggregatedStatistics.incomeByCategory.map(
-          (ic) => ic.colorCode,
-        ),
-      },
-    ],
-  }
+  public pieChartIncomeData!: ChartData<'pie'>;
 
   // Pie chart options and data for Expenses by Category
   public pieChartExpensesOptions: ChartOptions<'pie'> = {
     responsive: true,
   }
-  public pieChartExpensesData: ChartData<'pie'> = {
-    labels: this.aggregatedStatistics.expensesByCategory.map(
-      (ec) => ec.categoryName,
-    ),
-    datasets: [
-      {
-        data: this.aggregatedStatistics.expensesByCategory.map(
-          (ec) => ec.expenses,
-        ),
-        backgroundColor: this.aggregatedStatistics.expensesByCategory.map(
-          (ec) => ec.colorCode,
-        ),
-      },
-    ],
-  }
+  public pieChartExpensesData!: ChartData<'pie'>;
 
   dateRangeForm: FormGroup = this.fb.group({
     startDate: [''],
@@ -241,20 +54,13 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.setDefaultDates();
+    this.fetchData();
+    this.fetchPartnersData();
   }
 
   onSubmit() {
-    const { startDate, endDate } = this.dateRangeForm.value;
-    this.apiService
-      .post<AggregatedStatistics>(`/your-api-endpoint`, startDate)
-      .subscribe({
-        next: (data) => {
-          this.aggregatedStatistics = data;
-        },
-        error: (error) => {
-          console.error('Error fetching data', error);
-        },
-      });
+    this.fetchData();
+    
   }
 
   fetchPartnersData(){
@@ -272,20 +78,127 @@ export class DashboardComponent implements OnInit {
     });
   }
 
+  fetchData(){
+    let queryParameters = new HttpParams();
+    queryParameters = queryParameters.append('fromDate', this.formatDate(this.dateRangeForm.value.startDate));
+    queryParameters = queryParameters.append('toDate', this.formatDate(this.dateRangeForm.value.endDate)); 
+
+    this.apiService.get<AggregatedStatistics>('reporting/dashboard/', queryParameters).subscribe({
+      next: (data: AggregatedStatistics) => {
+        this.aggregatedStatistics = data;
+        console.log(data);
+        this.updateCharts();
+      },
+      error: (error) => {
+        console.error('Error fetching elements', error);
+      },
+      complete: () => {
+        console.info('elements data fetch complete');
+      }
+    });
+
+    this.getPartnerData();
+  }
+
+  getPartnerData() {
+    let queryParameters = new HttpParams();
+    queryParameters = queryParameters.append('fromDate', this.formatDate(this.dateRangeForm.value.startDate));
+    queryParameters = queryParameters.append('toDate', this.formatDate(this.dateRangeForm.value.endDate)); 
+    if (this.selectedPartnerId) {
+      this.apiService.get<CashFlowByPartner>(`reporting/dashboard/partner/${this.selectedPartnerId}`, queryParameters).subscribe({
+        next: (data: CashFlowByPartner) => {
+          this.aggregatedStatistics.cashFlowByPartner = data;
+          console.log(data);
+          this.updatePartnerChart();
+        },
+        error: (error) => {
+          console.error('Error fetching elements', error);
+        },
+        complete: () => {
+          console.info('elements data fetch complete');
+        }
+      });
+    }
+  }
+
+  updateCharts() {
+    this.updateBarChart();
+    this.updateIncomePieChart();
+    this.updateExpensesPieChart();
+  }
+
+  updateBarChart() {
+    this.barChartData = {
+      labels: this.aggregatedStatistics.cashFlow.map((cf) => cf.month),
+      datasets: [
+        {
+          data: this.aggregatedStatistics.cashFlow.map((cf) => cf.income),
+          label: 'Venituri',
+          backgroundColor: '#8B93FF',
+        },
+        {
+          data: this.aggregatedStatistics.cashFlow.map((cf) => cf.expenses),
+          label: 'Cheltuieli',
+          backgroundColor: '#FB7185',
+        },
+      ],
+    };
+  }
+
+  updateIncomePieChart() {
+    this.pieChartIncomeData = {
+      labels: this.aggregatedStatistics.incomeByCategory.map(
+        (ic) => ic.categoryName,
+      ),
+      datasets: [
+        {
+          data: this.aggregatedStatistics.incomeByCategory.map((ic) => ic.income),
+          backgroundColor: this.aggregatedStatistics.incomeByCategory.map(
+            (ic) => ic.colorCode,
+          ),
+        },
+      ],
+    };
+  }
+
+  updateExpensesPieChart() {
+    this.pieChartExpensesData = {
+      labels: this.aggregatedStatistics.expensesByCategory.map(
+        (ec) => ec.categoryName,
+      ),
+      datasets: [
+        {
+          data: this.aggregatedStatistics.expensesByCategory.map(
+            (ec) => ec.expenses,
+          ),
+          backgroundColor: this.aggregatedStatistics.expensesByCategory.map(
+            (ec) => ec.colorCode,
+          ),
+        },
+      ],
+    };
+  }
+
+  updatePartnerChart() {
+    // Implement the logic to update the partner-specific chart here
+    // This method will be called when partner data is fetched
+  }
+
   setDefaultDates(): void {
     const endDate = new Date();
     const startDate = new Date();
     startDate.setMonth(endDate.getMonth() - 6);
 
     this.dateRangeForm.patchValue({
-      startDate: startDate.toISOString().split('T')[0],
-      endDate: endDate.toISOString().split('T')[0],
+      startDate: startDate,
+      endDate: endDate,
     });
   }
 
-  getCashFlowByPartner(): void {
-    
+  formatDate(date: Date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
   }
-
-  
 }
